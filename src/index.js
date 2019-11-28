82a170e3097cac15400b5345aef4165f22e198ad
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-11-28 18:28:27 +0000

    Font style changed
    size and color

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className='bg'>
-        <div className='rcorners'>
-          <h4>REDEFINING MOBILE<br />
-            ADVERTISING</h4>
-          <p>Please use this form to register.<br />
-            If you are a member, please login.
-          </p>
+        <div className='main-box'>
+          <div className='header'>
+            <h4>REDEFINING MOBILE<br />
+              ADVERTISING</h4>
+            <p className='para'>Please use this form to register.<br />
+              If you are a member, please login.
+            </p>
+          </div>
         </div>
       </div>
     )

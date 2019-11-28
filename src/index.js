016549e3b4e79e6ef4bed13c16d875cde44e9b95
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-11-28 16:55:52 +0000

    Main box created and centered

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className='bg'>
-
+        <div className='rcorners'>
+          <h4>REDEFINING MOBILE<br />
+            ADVERTISING</h4>
+          <p>Please use this form to register.<br />
+            If you are a member, please login.
+          </p>
+        </div>
       </div>
     )
   }
